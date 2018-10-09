5aaac5a594fe9ed1deea22b06f10278168489419
Keith Bender <k.bender30@gmail.com>
2018-10-09 11:44:58 -0500

    finished

--- js/main.js
+++ js/main.js
@@ -1,27 +1,31 @@
 ScrollReveal().reveal('.sr');
 
-$(document).ready(function () {
-    $(this).scrollTop(0);
-});
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
 
 $("#menu").click(function () {
     toggleMenuBar();
 });
 
-$(".sliding-link-more").click(function(event){
+$(".sliding-link-more").click(function (event) {
     event.preventDefault();
     var aid = $(this).attr("href");
-    $('html,body').animate({scrollTop: $(aid).offset().top}, 'slow');
+    $('html,body').animate({
+        scrollTop: $(aid).offset().top
+    }, 'slow');
     toggleMenuBar();
 });
 
-$("a[href='#top']").click(function() {
-    $("html, body").animate({ scrollTop: 0 }, "slow");
+$("a[href='#top']").click(function () {
+    $("html, body").animate({
+        scrollTop: 0
+    }, "slow");
     toggleMenuBar();
     return false;
-  });
+});
 
-function toggleMenuBar () {
+function toggleMenuBar() {
     $("i.fas").fadeToggle(function () {
         $("i.fas").toggleClass("fa-bars fa-times");
     });
